748cf5e43910a30a37f99f570c98f94a36bea57b
EFFETTO <100048888+effettot@users.noreply.github.com>
2023-04-10 19:36:40 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,8 +1,16 @@
-def generate_password(length)
-  characters = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a + ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
+def generate_password(length, use_numbers=false, use_symbols=false)
+  characters = ('a'..'z').to_a + ('A'..'Z').to_a
+  characters += ('0'..'9').to_a if use_numbers
+  characters += ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'] if use_symbols
   password = ''
   length.times { password << characters[rand(characters.length)] }
   password
 end
 
-puts "La tua nuova password è: #{generate_password(12)}"
+puts "Vuoi includere numeri nella password? (S/N)"
+use_numbers = gets.chomp.downcase == 's'
+
+puts "Vuoi includere caratteri speciali nella password? (S/N)"
+use_symbols = gets.chomp.downcase == 's'
+
+puts "La tua nuova password è: #{generate_password(12, use_numbers, use_symbols)}"
